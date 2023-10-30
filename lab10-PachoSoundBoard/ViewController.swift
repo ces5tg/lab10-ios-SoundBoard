5d6d7c265bdc02f6ff84ff32089bcc68694fd8a3
ces5tg <cesar.pacho@tecsup.edu.pe>
2023-10-29 17:56:39 -0700

    Initial Commit

---
+++ lab10-PachoSoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab10-PachoSoundBoard
+//
+//  Created by cesar on 10/29/23.
+//  Copyright © 2023 cesar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
